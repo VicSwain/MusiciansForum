608bcf800ccff41a72f24b2b051e8eb217348a6a
VicSwain <huracanmusic2016@gmail.com>
2024-04-18 11:39:07 -0600

    added api folder to controllers and well as installed dependencies, server attempt but need to set up controllers

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,19 @@ const sequelize = process.env.DB_HOST
 : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    proce.env.DB_PASSWORD,
+    process.env.DB_PASSWORD,
     {
+        // sequelize options
         host: 'localhost',
         dialect: 'postgres',  
     }
+    
 );
+// try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
 
 module.exports = sequelize; 
